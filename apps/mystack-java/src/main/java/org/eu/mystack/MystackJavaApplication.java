4ebfc17d28f31f85106c3b0cf50e6e7762b54e9a
Dariusz Ochota <ochota.dariusz@gmail.com>
2024-06-07 22:42:15 +0200

    chore: initial backend files

---
+++ apps/mystack-java/src/main/java/org/eu/mystack/MystackJavaApplication.java
@@ -0,0 +1,13 @@
+package org.eu.mystack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MystackJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MystackJavaApplication.class, args);
+	}
+
+}
---
+++ apps/mystack-java/src/test/java/org/eu/mystack/MystackJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.eu.mystack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MystackJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
